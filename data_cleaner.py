304fd7ad98d6d33bbcede9a778f4addd27092354
Dieudonne Bahati <didopro2027@gmail.com>
2026-02-22 16:05:25 +0100

    Try-except valueeror in csv files

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,5 +2,11 @@ import csv
 
 with open("data/students.csv","r") as file:
 	reader = csv.DictReader(file)
+
 	for row in reader:
 		print(row)
+
+	try:
+		score = int(row["score"])
+	except ValueError:
+		print("Invalid score value:", row["score"])
--- read_csv_basic.py
+++ read_csv_basic.py
@@ -2,6 +2,11 @@ import csv
 
 with open("data/weather.csv", "r") as file:
     reader = csv.reader(file)
-    
+
+#to i learn how to read csv file using csv.reader() method, and to catch the error if the value in the score column is not an integer, we can use a try-except block to handle the ValueError that may occur when trying to convert the value to an integer. Here's how you can do it:      
     for row in reader:
         print(row)
+    try:
+        humidity = int(row["humidity"])
+    except ValueError:
+        print("Invalid humidity value:", row["humidity"])
